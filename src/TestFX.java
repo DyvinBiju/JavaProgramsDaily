b3a5a6c881ea2b7fd0dd63c48c09fb7926c8e06f
DyvinBiju <dyvinbiju@gmail.com>
2026-09-17 12:28:02 +0530

    Fx demo1

---
+++ src/TestFX.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("JavaFX Test");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
